605d9d3475d40c97a7cbf39cf4488a22e02396d1
alexjoo3391 <61110626+alexjoo3391@users.noreply.github.com>
2022-06-07 00:23:15 +0900

    Update script.js

--- jsp_project/WebContent/lib/script.js
+++ jsp_project/WebContent/lib/script.js
@@ -1,5 +1,10 @@
 window.onload = () => {
+	let str = document.querySelector('input[type=hidden]').value.splice(1, 0);
+	let cnt = 0;
 	document.addEventListener("keydown", (e) => {
-		console.log(e);
+		if(e.key = ' ' && str.length > cnt) {
+			document.querySelector('.text p').innerText = str[cnt];
+			cnt++;
+		}
 	});
 }
