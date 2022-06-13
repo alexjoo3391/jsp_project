201eaf4ca5587bab5448d6435f7d3be2fe096f12
seoyeon12345 <seoyeonchoe003@gmail.com>
2022-06-13 11:16:04 +0900

    zzfQQ

--- jsp_project/WebContent/query.sql
+++ jsp_project/WebContent/query.sql
@@ -1,14 +1,12 @@
 CREATE TABLE member (
 	id VARCHAR2(20) PRIMARY KEY,
 	password VARCHAR2(30),
-	money int,
-	item VARCHAR2(100),
 	check_point int,
 	love number(3)
 );
 
 SELECT * FROM member;
 
-INSERT INTO member VALUES ('dd', 'dd', 0, 'dd', 1, 0);
+INSERT INTO member VALUES ('dd', 'dd', 1, 0);
 
 DELETE FROM member WHERE id = 'aaaa';
